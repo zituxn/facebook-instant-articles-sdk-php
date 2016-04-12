1d5414a1f5070c0fde586a7465ef6a6d70749598
diegoquinteiro <diegoquinteiro@fb.com>
2016-04-11 21:21:58 -0300

    Bump version to v1.0.1

--- src/Facebook/InstantArticles/Elements/InstantArticle.php
+++ src/Facebook/InstantArticles/Elements/InstantArticle.php
@@ -33,7 +33,7 @@ use Facebook\InstantArticles\Validators\Type;
 */
 class InstantArticle extends Element
 {
-    const CURRENT_VERSION = '1.0.0';
+    const CURRENT_VERSION = '1.0.1';
 
     /**
      * The meta properties that are used on <head>
