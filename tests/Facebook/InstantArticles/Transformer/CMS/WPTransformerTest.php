a7f9ed86f225a8ea7b71f00c8bc0999368a1f7ed
Everton Rosario <erosario@gmail.com>
2016-06-14 15:09:58 -0300

    Diego's comments

--- tests/Facebook/InstantArticles/Transformer/CMS/WPTransformerTest.php
+++ tests/Facebook/InstantArticles/Transformer/CMS/WPTransformerTest.php
@@ -71,6 +71,7 @@ class WPTest extends \PHPUnit_Framework_TestCase
         $expected = file_get_contents(__DIR__ . '/wp-ia.xml');
 
         $this->assertEquals($expected, $result);
-        $this->assertEquals(0, count($transformer->getWarnings()));
+        // there must be 1 warning related to <img> inside <li> that is not supported by IA
+        $this->assertEquals(1, count($transformer->getWarnings()));
     }
 }
