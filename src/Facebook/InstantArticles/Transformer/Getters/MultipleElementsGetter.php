c32ee1160f8fca880c1178da9ff594513629cf9d
Diego Moreno Quinteiro <diegoquinteiro@gmail.com>
2016-07-22 18:36:21 -0300

    Add sibling and multiple getter to properly handly IG embeds

---
+++ src/Facebook/InstantArticles/Transformer/Getters/MultipleElementsGetter.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+namespace Facebook\InstantArticles\Transformer\Getters;
+
+use Facebook\InstantArticles\Validators\Type;
+use Symfony\Component\CssSelector\CssSelectorConverter;
+
+class MultipleElementsGetter extends AbstractGetter
+{
+    /**
+     * @var Getters
+     */
+    protected $children = [];
+
+    // TODO: make it static
+    public function createFrom($properties)
+    {
+        foreach ($properties['children'] as $getter_configuration) {
+            $this->children[] = GetterFactory::create($getter_configuration);
+        }
+        return $this;
+    }
+
+    public function get($node)
+    {
+        $fragment = $node->ownerDocument->createDocumentFragment();
+        foreach ($this->children as $child) {
+            $single_node = $child->get($node);
+            if (Type::is($single_node, 'DOMNode')) {
+                $fragment->appendChild($single_node->cloneNode(true));
+            }
+        }
+        if ($fragment->hasChildNodes()) {
+            return $fragment;
+        }
+        return null;
+    }
+}
---
+++ src/Facebook/InstantArticles/Transformer/Getters/NextSiblingElementGetter.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Copyright (c) 2016-present, Facebook, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+namespace Facebook\InstantArticles\Transformer\Getters;
+
+use Facebook\InstantArticles\Validators\Type;
+
+class NextSiblingElementGetter extends ElementGetter
+{
+    public function createFrom($properties)
+    {
+        if (isset($properties['selector'])) {
+            $this->withSelector($properties['selector']);
+        }
+        if (isset($properties['attribute'])) {
+            $this->withAttribute($properties['attribute']);
+        }
+    }
+
+    public function get($node)
+    {
+        Type::enforce($node, 'DOMNode');
+        $elements = self::findAll($node, $this->selector);
+        if (!empty($elements) && $elements->item(0)) {
+            $element = $elements->item(0);
+            do {
+                $element = $element->nextSibling;
+            } while ($element !== null && !Type::is($element, 'DOMElement'));
+
+            if ($element) {
+                return $element;
+            }
+        }
+        return null;
+    }
+}
