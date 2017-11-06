47f7971598278b95269dd848e762c4f24e87cd05
Matias Burak <mburak@users.noreply.github.com>
2017-11-06 14:59:23 -0300

    made UnrecognizedElement HHVM strict - fixed lints

--- src/Facebook/InstantArticles/Transformer/Warnings/UnrecognizedElement.php
+++ src/Facebook/InstantArticles/Transformer/Warnings/UnrecognizedElement.php
@@ -11,42 +11,48 @@ namespace Facebook\InstantArticles\Transformer\Warnings;
 use Facebook\InstantArticles\Elements\Element;
 use Facebook\InstantArticles\Validators\Type;
 
-class UnrecognizedElement extends TransformerWarning
-{
-    /**
-     * @param Element $context
-     * @param \DOMNode $node
-     */
-    public function __construct($context, \DOMNode $node)
-    {
-        parent::__construct(null, $context, $node ? $node->cloneNode(): null, null);
-    }
+class UnrecognizedElement extends TransformerWarning {
+  /**
+   * @param Element $context
+   * @param \DOMNode $node
+   */
+  public function __construct($context, \DOMNode $node) {
+    parent::__construct(
+      null,
+      $context,
+      $node ? $node->cloneNode() : null,
+      null,
+    );
+  }
 
-    /**
-     * @return string
-     */
-    public function __toString()
-    {
-        $reflection = new \ReflectionClass(get_class($this->getContext()));
-        $className = $reflection->getShortName();
-        if ($this->getNode()) {
-            $nodeName = $this->getNode()?->getNode();
-        } else {
-          $nodeName = null;
-        }
-        if (substr($nodeName, 0, 1) === '#') {
-            $nodeDescription = '"'.mb_strimwidth($this->getNode()?->textContent, 0, 30, '...').'"';
-        } else {
-            $nodeDescription = '<';
-            $nodeDescription .= $nodeName;
-            if ($this->getNode() instanceof \DOMNode) {
-                $class = $this->getNode()?->getAttribute('class');
-                if ($class) {
-                    $nodeDescription .= ' class="'. $class .'"';
-                }
-            }
-            $nodeDescription .= '>';
-        }
-        return "No rules defined for {$nodeDescription} in the context of $className";
+  /**
+   * @return string
+   */
+  public function __toString() {
+    $reflection = new \ReflectionClass(get_class($this->getContext()));
+    $className = $reflection->getShortName();
+    $elem = $this->getNode();
+    if ($elem instanceof DOMElement) {
+      $nodeName = $elem->getNode();
+    } else {
+      $nodeName = null;
     }
+    if (substr($nodeName, 0, 1) === '#') {
+      $nodeDescription =
+        '"'.mb_strimwidth($this->getNode()?->textContent, 0, 30, '...').'"';
+    } else {
+      $nodeDescription = '<';
+      $nodeDescription .= $nodeName;
+      $elem = $this->getNode();
+      if ($elem instanceof DOMElement) {
+        $class = $elem->getAttribute('class');
+        if ($class) {
+          $nodeDescription .= ' class="'.$class.'"';
+        }
+      }
+      $nodeDescription .= '>';
+    }
+    return
+      "No rules defined for {$nodeDescription} in the context of $className";
+  }
 }
